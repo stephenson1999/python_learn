c288d457e99587790e8b1ca7056bfca84ca30bb0
Scooter X <ScooterX@hotmail.com>
2025-10-09 07:38:58 -0700

    Changed main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-while True:
-    n = int(input("Enter a number or:")) 
-    def fibonacci_numbers(n):
-        L = []
-        if n == 0:
-            L = [0]
-            return L 
-        if n == 1:
-            L = [0, 1]
-            return L
-        L = [0, 1]
-        for i in range(n+1):
-            if i > 1:
-                f = L[i-1]+L[i-2]
-                L.append(f)
-        return L
-    L1 = fibonacci_numbers(n)
-    print (L1)
+my_tuply = ()
+print(my_tuply)
+my_tuply = (1,2,3)
+print(my_tuply)
+my_tuply = (1,2,3, "hi")
+print(my_tuply)
+my_tuply = (1,2,3,"hi",4,5)
+print(my_tuply)
+my_tuply = (1,2,3, "hi")
+print(my_tuply[0])
+print(my_tuply[1])
+print(my_tuply[2])
+print(my_tuply[0:3])
+
+my_set = {1,2,3,4,5,6,7,8,9}
+print(my_set)
+my_set.add(10)
+print(my_set)
+
+set_1 = my_set
+set_2 = {10,1,11,12,9}
+print(set_1.difference(set_2))
+
+setun_1 = {"green", "yellow"}
+setun_2 = {"yellow", "blue"}
+print("unifn: ", setun_1.union(setun_2))
+fruit_1 = {"apple","banana"}
+fruit_2 = {"banana", "strawberry"}
+print(fruit_1.intersection(fruit_2))
