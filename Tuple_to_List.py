6fe95a185ffab7fa9c990c55c872a3f3afa9743d
Scooter X <ScooterX@hotmail.com>
2025-10-11 15:02:41 -0700

    Added Tuple_to_List.py

---
+++ Tuple_to_List.py
@@ -0,0 +1,7 @@
+def tuple_to_list():
+    my_tuple = ("Jame", "Backer", "28", "56 inch", "150 pounds", "computer")
+    my_list = list(my_tuple)  
+    print(type(my_list))
+    print(my_list)
+
+tuple_to_list()
