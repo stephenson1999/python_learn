688480907a5338f6bdd9f4e2319f9583479c460d
Scooter X <ScooterX@hotmail.com>
2025-10-13 07:48:30 -0700

    Added bug code

---
+++ main.py
@@ -0,0 +1,28 @@
+# class parrort:
+#     def __init__(self):
+#         self.name = "bebebebabebebebabebebebabebebebabebebebabebebeba"
+#         self.age = 5
+#         self.color = "verde, amaillo, rojo"
+#         self.height = 12
+#         self.weight = 1
+#     def print_parret(self):
+#         print(self.name)
+#         print(self.age, "")
+#         print(self.color)
+#         print(self.weight)
+
+# ob = parrort()
+# ob.print_parret()
+class parrot:
+    def __init__(self, name, age):
+        self.name = "bebebebebebebebebebeb"
+        self.age = "99999999999999999999999999999999999999999999999999999 year's old"
+    
+    def dance(self):
+        print(self.name, "is dancing")
+    def sing(self):
+        print(self.age, self.name, "is sing!")
+
+obs = parrot
+obs.dance()
+obs.sing()
