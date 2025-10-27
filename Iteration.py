3da172846a7c83f0159e2e19da2a02fe815dbf17
Scooter X <ScooterX@hotmail.com>
2025-10-27 16:32:40 -0700

    added Iteraton.py

---
+++ Iteration.py
@@ -0,0 +1,17 @@
+num1 = int(input("Num 1 : "))
+num2 = int(input("Num 2 : "))
+
+def i_mutipler(num1, num2):
+    for i in range(1):
+        total = num1 * num2
+        print(total)
+
+n = 5
+
+def n_mutipler(num1, num2):
+    for j in range(n):
+        total = num1 * num2
+        print(total)
+
+i_mutipler(num1=num1, num2=num2)
+n_mutipler(num1=num1, num2=num2)
