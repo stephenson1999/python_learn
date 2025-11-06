1397eaa3e9b8f2c2e401c2195a27fb232dd7e7e8
Scooter X <ScooterX@hotmail.com>
2025-11-06 07:00:32 -0800

    added main.py

--- main/main.py
+++ main/main.py
@@ -1,27 +1,77 @@
-def prints(n):
-    if (n<=0):
-        return
-    print("Codingal")
-    print(n/2)
-    for _ in range(1, 10):
-        prints(n/2)
+# #one
+# num = 407
+# print("Number:", num)
+# dights = len(str(num))
+# print("Dights: ", dights)
+# resutle = 0
+# temp = num
+# while temp>0:
+#     dights=temp%10
+#     resutle+=dights**dights
+#     temp//=10
+#     if num == resutle:
+#         print(f"{num} is a armstrong number")
+#     else:
+#         print("NO")
+    
+# #two
+# def print_factors(number):
+#     print("Factors:")
+#     for i in range(1, number+1):
+#         if number%i==0:
+#             print(i)
 
-prints(10)
+# number = int(input("Number: "))
+# print_factors(number)
 
-def sum(n):
-    return n*(n+1)/2
+#there
 
-def arrey_sum(a):
-    sum = 0
-    for i in a:
-        sum = sum+i
-    return sum
 
-a = [12,325,3,12,2,4,3,4,632,234,456,2,4,1,5,245]
 
-arrey_sum(a)
+def int_to_roman(number):
 
-def sums(n):
-    if (n<=0):
-        return
-    return n+ sum(n+1)
+    val = [
+
+1000, 900, 500, 400,
+
+100, 90, 50, 40,
+
+10, 9, 5, 4,
+
+1
+
+]
+
+    syb = [
+
+"M", "CM", "D", "CD",
+
+"C", "XC", "L", "XL",
+
+"X", "IX", "V", "IV",
+
+"I"
+
+]
+
+    roman_num = ''
+
+    i = 0
+
+    while number > 0:
+
+        for _ in range(number // val[i]):
+
+            roman_num += syb[i]
+
+    number -= val[i]
+
+    i += 1
+
+    return roman_num
+
+print("{} in Roman Numerals is {}".format(200, int_to_roman(200)))
+
+            
+
+
