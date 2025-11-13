a5065f6e6afe85a5df6d83238c6f9320295212e3
Scooter X <ScooterX@hotmail.com>
2025-11-13 07:09:21 -0800

    ok

--- main/main.py
+++ main/main.py
@@ -1,31 +1,45 @@
-num  = int(input("Enter a number: "))
-original = num
-reversed_num = 0
-while num>0:
-    dight = num/10
-    reversed_num = reversed_num*10+dight
-    num //= 10
+# #Activity 1
+# num = int(input("Enter a number: "))
+# s = num/2
+# fractional_part = str(int(str(s).split(".")[1]))
+# if num == 2:
+#     print("The number is a prime number!")
+# elif fractional_part == "0":
+#     print("This number is not a prime number!")
+# else:
+#     print("The number is a prime number!")
 
-if  original == reversed_num:
-    print("Padorim")
-else:
-    print("No")
+#Acitivity 2
+
+def Sieve_of_Eratosthenes(num):
+
+    prime = [True for i in range(num + 1)]
+
+    p = 2
+
+    while p * p <= num:
+
+        if prime[p]:
+
+            for i in range(p * p, num + 1, p):
+
+                prime[i] = False
+
+                p += 1
+
+            for p in range(2, num + 1):
+
+                if prime[p]:
+                    print(p, end=" ")
+
+num = int(input("Enter a number: "))
+
+Sieve_of_Eratosthenes(num)
 
 
 
 
 
 
-
-
-
-
-
-larger = int(input("Larger: "))
-smaller = int(input("Smaller: "))
-while (smaller):
-    number_Store = smaller
-    smallest = larger%smaller
-    larger = number_Store
-    print("GCD: ", larger)
-    print("HCD: ", larger)
+    
+    
