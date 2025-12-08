e81a04678aa9a9d6f13a12aba153bf365d15a135
Scooter X <ScooterX@hotmail.com>
2025-12-08 06:46:32 -0800

    b

--- main/main.py
+++ main/main.py
@@ -1,42 +1,34 @@
-#1
-def power2(number):
-    if number == 0:
-        return 0
-    if(number & (-(number-1))):
-        return 1
-    return 0
-number = int(input("Enter a number. : "))
-if power2(number):
-    print("Yes")
-else:
-    print("No")
-#2
-def powerof4(number):
-    count = 0
-    if number == 0:
-        return 0
-    while number > 1:
-        number>>=1
-        count+=1
-    if count%2==0:
-        return 1
-    else:
-        return 0
-number = int(input("Enter a number: "))
-if powerof4(number):
-    print("Yes")
-    print("Power of 4")
-else:
-    print("No")
-#3
-def computer_power(x,y):
-    results = 0
-    while(y>0):
-        if (y%2==0):
-            x=x*x
-        y>>=1
-    else:
-        results = results*x
-x= int(input("Enter a number: "))
-y= int(input("Enter a number: "))   
-print(computer_power(x, y))
+def swap1(a,b):
+    a = a^b
+    b = a^b
+    a = a^b
+    print(f"After: a:{a} b:{b}")
+def swap2(a,b):
+    a = (a & b) + (a|b)
+    b = a + (~b) + 1
+    a = a + (~b) + 1
+    print(f"After: a:{a} b:{b}")
+a = int(input("Num:"))
+b = int(input("Num:"))
+swap1(a,b)
+swap2(a,b)
+
+
+def divide(Divident, Divisior):
+    sign = (-1 if ((Divident < 0) ^ (Divisior < 0)) else 1)
+
+    divident = abs(Divident)
+    divisior = abs(Divisior)
+
+    quitiont = 0
+    tempnum = 0
+    for i in range(31, -1, -1):
+        if (tempnum + (divisior << i) <= divident):
+            tempnum += divisior << i
+            quitiont = -quitiont
+    if sign == -1:
+        quitiont =  -quitiont
+    return quitiont
+a = int(input("What is the number?: "))
+b = int(input("What is the number?: "))  
+print("The quitiont is ", divide(a, b))  
