11009761ef41b677c2439e0e0f7985ec3daff2ed
Scooter X <ScooterX@hotmail.com>
2025-12-11 06:48:11 -0800

    Dec/11/25

--- main/main.py
+++ main/main.py
@@ -1,34 +1,33 @@
-def swap1(a,b):
-    a = a^b
-    b = a^b
-    a = a^b
-    print(f"After: a:{a} b:{b}")
-def swap2(a,b):
-    a = (a & b) + (a|b)
-    b = a + (~b) + 1
-    a = a + (~b) + 1
-    print(f"After: a:{a} b:{b}")
-a = int(input("Num:"))
-b = int(input("Num:"))
-swap1(a,b)
-swap2(a,b)
+import math
+def print_power(set, SetSize):
+    PowerSetSize = (int)(math.pow(2, SetSize))
+    outer=0
+    inner=0
+    for outer in range(0,PowerSetSize):
+        for inner in range(0, SetSize):
+            if (outer & (1<<inner)):
+                print(set[inner], end="")
+            print("")
+
+size = int(input("Size: "))
+set = []
+for i in range(0, size):
+    element = int(input("Enter Element:"))
+    set.append(element)
+print_power(set, size)
 
 
-def divide(Divident, Divisior):
-    sign = (-1 if ((Divident < 0) ^ (Divisior < 0)) else 1)
-
-    divident = abs(Divident)
-    divisior = abs(Divisior)
-
-    quitiont = 0
-    tempnum = 0
-    for i in range(31, -1, -1):
-        if (tempnum + (divisior << i) <= divident):
-            tempnum += divisior << i
-            quitiont = -quitiont
-    if sign == -1:
-        quitiont =  -quitiont
-    return quitiont
-a = int(input("What is the number?: "))
-b = int(input("What is the number?: "))  
-print("The quitiont is ", divide(a, b))  
+def flip(num1, num2):
+    flip = 0
+    while (num1 > 0 or num2 > 0):
+        t1 = num1 & 1
+        t2 = num2 & 1
+        if t1 != t2:
+            flip += 1
+        num1 >>= 1
+        num2 >>= 1
+    return flip
+num1 = int(input("Enter: "))
+num2 = int(input("Enter: "))
+print("Flips needed to be the same: ", flip(num1, num2))
+    
