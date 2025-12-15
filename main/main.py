90fc9a02e0185b4886bc6dcc096d69e2fb77ffc3
Scooter X <ScooterX@hotmail.com>
2025-12-15 06:44:25 -0800

    added to main

--- main/main.py
+++ main/main.py
@@ -1,33 +1,29 @@
-import math
-def print_power(set, SetSize):
-    PowerSetSize = (int)(math.pow(2, SetSize))
-    outer=0
-    inner=0
-    for outer in range(0,PowerSetSize):
-        for inner in range(0, SetSize):
-            if (outer & (1<<inner)):
-                print(set[inner], end="")
-            print("")
-
-size = int(input("Size: "))
-set = []
-for i in range(0, size):
-    element = int(input("Enter Element:"))
-    set.append(element)
-print_power(set, size)
+def print_num(n):
+    if (n <= 10):
+        print(n)
+        print_num(n+1)
+    else:
+        return
+print_num(1)
 
 
-def flip(num1, num2):
-    flip = 0
-    while (num1 > 0 or num2 > 0):
-        t1 = num1 & 1
-        t2 = num2 & 1
-        if t1 != t2:
-            flip += 1
-        num1 >>= 1
-        num2 >>= 1
-    return flip
-num1 = int(input("Enter: "))
-num2 = int(input("Enter: "))
-print("Flips needed to be the same: ", flip(num1, num2))
-    
+
+num = int(input("Enter a number "))
+
+def recursion(n):
+    recursions = 0
+    start = 1
+    end = 1
+    if num == 0:
+        print("0")
+    if recursions != n:
+        swich_num = start * end
+
+
+
+def fac(n):
+    if n==0 or n==1:
+        return 1
+    return n*fac(n-1)
+n = int(input("Enter a number?: "))
+print(fac(n))
