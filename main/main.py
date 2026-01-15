6fc1881f6ce57ed3d59a080bf6d43afd14da1a2a
Scooter X <ScooterX@hotmail.com>
2026-01-15 06:44:47 -0800

    Jan 15 2026

--- main/main.py
+++ main/main.py
@@ -1,29 +1,48 @@
-list = ["apple", "cherry", "bannana", "grapefruit", "enlderberry"]
-sorted = list.sort()
-if list == sorted:
-    print("the list is sorted")
-else:
-    print("the list is not sorted")
-
-
-list1 = [1,2,3,4,5,6,7]
-list2 = [2,2,3,6,43,5,34,6,23]
-len1 = len(list1)
-len2 = len(list2)
-sum = len1 + len2
-print(sum)
-
-
-def largest(n, current_max=None):
-    if n == 0:
-        return current_max
-    num = int(input("Enter Number: "))
-
-    if current_max is None or num > current_max:
-        current_max = num
-
-    return largest(n-1, current_max)
-
-count = int(input("how many numbers"))
-largest = print("largest element: ", largest(count))
+#1
+def reverse_num(num):
+    if (num>0):
+      last = num%10
+      if (num//10>0):
+         current_number = reverse_num((int)(num//10))
+         return last*pow(10, len(str(current_number)))+current_number
+      else:
+         return last
+    else:
+       return 0
     
+num = int(input("Enter a number: "))
+print("Reverse num : ", reverse_num(num))
+
+
+#2
+def reverse_str(s):
+   if len(s) == 1:
+      return s[0]
+   
+   fist_char = s[0]
+   return(reverse_str(s[1:])) + fist_char
+
+s = str(input("Enter a string: "))
+print("Reversed string: ", reverse_str(s))
+
+
+#3
+def powerof4(number):
+   count = 0
+   if number==0:
+      return 0 
+   while num > 1:
+      number>>=1
+      count+=1
+   if count%2==0:
+      return 1
+   else: 
+      return 0
+   
+
+number = int(input("What is the number?: "))
+if powerof4(number):
+   print("yes")
+   print("It is a power of four.")
+else:
+   print("No it is not a power of four.")    
