85230cd6fcccea801c928ae402229125cec517bc
Scooter X <ScooterX@hotmail.com>
2026-01-19 06:34:40 -0800

    19 / Jan / 2026

--- main/main.py
+++ main/main.py
@@ -1,48 +1,34 @@
-#1
-def reverse_num(num):
-    if (num>0):
-      last = num%10
-      if (num//10>0):
-         current_number = reverse_num((int)(num//10))
-         return last*pow(10, len(str(current_number)))+current_number
-      else:
-         return last
-    else:
-       return 0
-    
-num = int(input("Enter a number: "))
-print("Reverse num : ", reverse_num(num))
+#1  
+def ways(stairs):
+    if stairs<0:
+        return 0 
+    if stairs==1:
+        return 1
+    two_steps = 0
+    one_step = 0
+    three_steps = 0
+    if (stairs>=2):
+        two_steps=ways(stairs-1)
+    one_step=ways(stairs-1)
+    return one_step+two_steps+three_steps
+stairs = int(input("Enter a number : "))
+print("possoblitys :", ways(stairs))
+
+def paren(s,l,r,p,n):
+    if (p==2*n):
+        for ss in s:
+          print(ss, end=" ")
+        print("\n")
+        return
+    if l>r:
+        s[p] = "}"
+        paren(s,l,r+1,p+1,n)
+    if l<n:
+        s[p] = "{"
+        paren(s,l+1,r,p+1,n)
 
 
-#2
-def reverse_str(s):
-   if len(s) == 1:
-      return s[0]
-   
-   fist_char = s[0]
-   return(reverse_str(s[1:])) + fist_char
-
-s = str(input("Enter a string: "))
-print("Reversed string: ", reverse_str(s))
-
-
-#3
-def powerof4(number):
-   count = 0
-   if number==0:
-      return 0 
-   while num > 1:
-      number>>=1
-      count+=1
-   if count%2==0:
-      return 1
-   else: 
-      return 0
-   
-
-number = int(input("What is the number?: "))
-if powerof4(number):
-   print("yes")
-   print("It is a power of four.")
-else:
-   print("No it is not a power of four.")    
+n = int(input("Enter a number: "))
+s=['']*2*n
+print("\n")
+paren(s,0,0,0,n)
