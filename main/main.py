c8ff8cbedd6fa92dd342b27b2953fe77906b6ad7
Scooter X <ScooterX@hotmail.com>
2026-01-22 06:54:07 -0800

    Jan 22 26

--- main/main.py
+++ main/main.py
@@ -1,34 +1,30 @@
-#1  
-def ways(stairs):
-    if stairs<0:
-        return 0 
-    if stairs==1:
-        return 1  
-    two_steps = 0
-    one_step = 0
-    three_steps = 0
-    if (stairs>=2):
-        two_steps=ways(stairs-1)
-    one_step=ways(stairs-1)
-    return one_step+two_steps+three_steps
-stairs = int(input("Enter a number : "))
-print("possoblitys :", ways(stairs))
-
-def paren(s,l,r,p,n):
-    if (p==2*n):
-        for ss in s:
-          print(ss, end=" ")
-        print("\n")
+keypad = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+def printcombonation(cobination, curr, output, n):
+    if curr == n:
+        print(output)
         return
-    if l>r:
-        s[p] = "}"
-        paren(s,l,r+1,p+1,n)
-    if l<n:
-        s[p] = "{"
-        paren(s,l+1,r,p+1,n)
+    for i in range(len(keypad[cobination[curr]])):
+        output.append(len(keypad[cobination[curr]]))
+        printcombonation(cobination,curr+1,output, n)
+        output.pop()
+        if cobination[curr]==0 or cobination[curr]==1:
+            return
+combnation = [3,2,6]
+n=len(combnation)
+printcombonation(combnation, 0, [], n)
 
 
-n = int(input("Enter a number: "))
-s=['']*2*n
-print("\n")
-paren(s,0,0,0,n)
+def tower(n, source, destanation, auxiliary):
+    if n == 1:
+        print("Disk1 from source", source,"to ",destanation)
+        return
+    tower(n-1, source, destanation, auxiliary)
+    print(f"Move Disk {n} from source", source,"to ",destanation)
+    tower(n-1, source, destanation, auxiliary)
+
+n = 3
+tower(n, 'A', 'B', 'C')
+    
+
+    
+
