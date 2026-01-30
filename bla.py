f5ffd2df51fa81c1688d5dada6e53d538e9c4046
Scooter X <ScooterX@hotmail.com>
2026-01-29 16:01:31 -0800

    Jan 29 2026

--- bla.py
+++ bla.py
@@ -1,10 +1,14 @@
-array = [3, 23,5,3,3,2,5,4,42,3,4,24,32,4,2,342,4,2,3,7,454,5,64,7,32,42,3,4423,6,45,63,67]
-index = len(array) - 1
-new_array = []
+array = [0,2,4,6,8,10,14,16,18,20]
+wrong = None
+num = 0
+start = 0
+while wrong != True:
+    if num != array[start]:
+        wrong = True
+        num += 2
+    else:
+        start += 1
+        num += 2
 
-while index >= 0:
-    item = array[index]
-    new_array.append(item)
-    index -= 1
-
-print(new_array)
+if wrong == True:
+    print("Wrong number detected, right number: ", num)
