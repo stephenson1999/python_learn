f0cf07612e21d065bea928f7b2a10a15f5a9caad
Scooter X <ScooterX@hotmail.com>
2026-02-05 06:40:16 -0800

    Feb 5 2026

--- main/main.py
+++ main/main.py
@@ -1,30 +1,45 @@
-def caculate_profit(arr,a_size):
-    profit = 0
-    for i in range(1,a_size):
-        if arr[i] > arr[i-1]:
-            profit += arr[i] - arr[i-1]
+def maxlenth(a, a_size):
 
-    return profit
+    counter = 0
+    max_ones = 0
 
-prices = [234,4,53,34,534,53,453,4,65,7,87656,345765,34567543,234,54645,2342,765676,234234,6757,2342,676767]
+    for i in range(0, a_size):
 
-profit = caculate_profit(prices, len(prices))
-print("Max profit: ", profit)
+        if (a[i] == 0):
+            counter = 0
 
 
-def find_water(a,a_size):
-    left_tallest = [0]*a_size
-    right_tellest = [0]*a_size
-    water = 0
-    left_tallest[0] = a[0]
-    for i in range(1,a_size):
-        left_tallest[i] = max(left_tallest[i-1], a[i])
-    right_tellest[a_size-1] = a[a_size-1]
-    for i in range(1,a_size):
-        water += min(left_tallest[i], right_tellest[i]) - a[i]
-    return water
+        else: 
+            counter += 1
+            max_ones = max(max_ones, counter)
 
-a = [0,10101,1,0,3,5,6,6,4,7,9,5,3,56,8,5,3,5,7,9,6,4,670000,00000,0,0,0,0,0,0,0,0,0]
-bars = len(a)
-print("Water: ",find_water(a,bars))
-print("Bars: ", bars) 
+        return max_ones
+
+a = [1,0,0,0,1,1,1,1,0,1,0]
+a_size = len(a)
+print("Max ones: ", maxlenth(a, a_size))
+
+
+
+
+
+
+
+
+
+
+array = [1,0,1,0,1,34,2,6,43,90,756,4,0,54,6,9,43,0,45,0,64]
+lenth = int(len(array))
+zeros = []
+start = 0
+
+for i in range(0, lenth):
+    num = array[start]
+    if num == 0:
+        array.pop(start)
+        zeros.append(0)
+    else:
+        start += 1
+
+array.append(zeros)
+print(array)
