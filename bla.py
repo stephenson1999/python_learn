005c4458daa22ab99c7a2f5e4434b527f5fc7736
Scooter X <ScooterX@hotmail.com>
2026-02-23 06:45:58 -0800

    Feb 23 2026

--- bla.py
+++ bla.py
@@ -1,30 +1,59 @@
-def binary_search(arr,l,r,x):
-    while(l <= r):
-        mid = l + (r - l) //  2
+def is_pair(A,N,X):
+    for i in range(N):
+        for j in range(N):
+            if(i==j):
+                continue
+            if(A[i] + A[j] == X):
+                return(A[i] ,A[i])
+            if (A[i]+A[j]<X):
+                break
 
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] < x:
-            t = mid + 1
-        else:
-            r = mid -1
+    return 0
 
-    return -1
+arr = [1,5,2,6,4,8,5,8,4,7,4,6,5,3,8,4,6,42,4,7,2,4,5,3,6,8,4,3,4,6,34,7,4]
+val = 1253
+print("pair: {} {}".format(val, is_pair(arr,len(arr), val)))
 
-arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,172,91235871239587129357,928739182730491823]
-x = 91235871239587129357
+def is_pair_smells(A,N,X):
+    i = 0
+    j = N -1
+    while(i < j):
+        if(A[i] + A[j] == X):
+            return[A[i], A[j]]
+        if (A[i]+A[j]<X):
+            i += 1
 
-result = binary_search(arr, 0, len(arr)-1, x)
-
-if result != -1:
-    print("Element {} is present at index {}.".format(x, result))
-
-else:
-    print("NO")
-
-
+arr = [1,5,2,6,4,8,5,8,4,7,4,6,5,3,8,4,6,42,4,7,2,4,5,3,6,8,4,3,4,6,34,7,4]
+val = 12
+print("pair: {} {}".format(val, is_pair(arr,len(arr), val)))
+        
 
 
 
 
-cordnets = [-77828.74, 71.00, -45642.17]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
