0343414a19d195e8f705610f63ee9270f0450354
Scooter X <ScooterX@hotmail.com>
2026-02-25 21:10:21 -0800

    Feb 25 2026

--- bla.py
+++ bla.py
@@ -1,41 +1,15 @@
-def is_pair(A,N,X):
-    for i in range(N):
-        for j in range(N):
-            if(i==j):
-                continue
-            if(A[i] + A[j] == X):
-                return(A[i] ,A[i])
-            if (A[i]+A[j]<X):
-                break
+arr = [59, 12, 97, 41, 6, 73, 29, 84, 15, 62, 3, 90, 47, 21, 76, 34, 68, 9, 55, 100]
+current_small = [0, 0, float('inf')]  
 
-    return 0
+for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):  
+        a = arr[i]
+        b = arr[j]
+        c = abs(a - b)  
+        if c < current_small[2]: 
+            current_small = [a, b, c]
 
-arr = [1,5,2,6,4,8,5,8,4,7,4,6,5,3,8,4,6,42,4,7,2,4,5,3,6,8,4,3,4,6,34,7,4]
-val = 1253
-print("pair: {} {}".format(val, is_pair(arr,len(arr), val)))
-
-def is_pair_smells(A,N,X):
-    i = 0
-    j = N -1
-    while(i < j):
-        if(A[i] + A[j] == X):
-            return[A[i], A[j]]
-        if (A[i]+A[j]<X):
-            i += 1
-
-arr = [1,5,2,6,4,8,5,8,4,7,4,6,5,3,8,4,6,42,4,7,2,4,5,3,6,8,4,3,4,6,34,7,4]
-val = 12
-print("pair: {} {}".format(val, is_pair(arr,len(arr), val)))
-        
-
-
-
-
-
-
-
-
-
+print(current_small)
 
 
 
