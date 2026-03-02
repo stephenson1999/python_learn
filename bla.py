e195d55492e5730b1bc44d60b8b22c5f18d7edac
Scooter X <ScooterX@hotmail.com>
2026-03-02 06:47:04 -0800

    Mar 2 2026

--- bla.py
+++ bla.py
@@ -1,20 +1,36 @@
-arr = [11, 10, -20, 5, -3, -5, 8, -13, 10]
+def compare(n1,n2):
+    return str(n1) + str(n2) > str(n2) + str(n1)
 
-def bubble_search2(arr):
-    n = len(arr)
-    for i in range(n):
-        swap = False
-        for j in range(0,n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swap = True
-    if not swap:
-        return
+def Largest(nums):
+    for i in range(len(nums),0, -1):
+        tep = 0
+        for j in range(i):
+            if not compare(nums[j], nums[tep]):
+                tep = j
+        nums[tep], nums[i-1] = nums[i-1], nums[tep]
+    return str(int("".join(map(str,nums))))
 
-bubble_search2(arr)
-print("Sorted Array: ")
-for i in range(len(arr)):
-    print("%d"% arr[i], end=" ")
+arr = [2,7,3,12347,4,234,74,5,123,856]
+print("Given Array: ", arr)
+print("Largest Possible Number: ", Largest(arr))
+
+
+
+A = [64,26,36,14,52]
+for i in range(len(A)):
+    min_id = i
+    for j in range(i+1,len(A)):
+        if A[min_id] > A[j]:
+            min_id = j
+
+
+
+    A[i], A[min_id] = A[min_id],A[i]
+
+print("Sorted: ")
+for i in range(len(A)):
+    print("%d" %A[i], end=" ")
+
 
 
 
--- main/main.py
+++ main/main.py
@@ -1,31 +1,17 @@
-def seach(arr,n,x):
-    for i in range(0,n):
-        if [arr] == x:
-            return i
+
+
+a = ["3","8","2","23","64","12"]
+for i in range(len(a)):
+    if len(a[i]) > 1:
+        s = a[i]
+        a.pop(a[i])
+        q, r = divmod(len(s), 2)
+        first, second = s[:q + r], s[q + r:]
+
+        a.append(first)
+        a.append(second)
+
+
+
         
 
-    return -1
-array = [1,90,2,8,32,78,23,7]
-x = 78
-n = len(array)
-
-result = seach(array,n,x)
-if result == 0:
-    print("Element found")
-else:
-    print("Element Lost")
-
-cityes = [1,2,3,5,6,13,8,3,6,2,45,7,3]
-repets = 0
-
-
-for i in range(0, len(cityes)):
-    num =  cityes[i]
-    for n in range(0, len(cityes)):
-        gusts = cityes[n]
-        if num == gusts:
-            repets+=1
-            gusts = None
-
-print(repets)
-print(cityes)
