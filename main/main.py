1f781880ddfc49a583d853dcc131aaf3fa3f12d6
Scooter X <ScooterX@hotmail.com>
2026-03-02 16:18:06 -0800

    Mar 2 2026

--- main/main.py
+++ main/main.py
@@ -1,17 +1,5 @@
-
-
-a = ["3","8","2","23","64","12"]
-for i in range(len(a)):
-    if len(a[i]) > 1:
-        s = a[i]
-        a.pop(a[i])
-        q, r = divmod(len(s), 2)
-        first, second = s[:q + r], s[q + r:]
-
-        a.append(first)
-        a.append(second)
-
-
-
+arr = [9,3,6,2]
+arr.sort()
+print(arr)
         
 
