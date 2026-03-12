e5b75dad9667c07cc7e4d8a92530dea56c2699a6
Scooter X <ScooterX@hotmail.com>
2026-03-12 07:48:47 -0700

    Mar 12 2026

--- main/bla.py
+++ main/bla.py
@@ -1,57 +1,26 @@
-array = [2,6,3,6,2,7,4,3,8,2,6,4,5]
-# array.sort()
-# new_arr = []
-# print(array)
-# lenth = 0
-# index = 0
-# num = 0
-# num_count = 0
-# for i in range(len(array)):
-#     if len(str(array[i])) > lenth:
-#         num = array[i]
-#         index = i
-        
-#     for j in range(len(array)):
-#         start = array[i+1]
-#         if start == num:
-#             new_arr.append(num)
-# print(new_arr)
+A = [10,5,13,8,2]
+for i in range(1,len(A)):
+    value = A[i]
+    j = i-1
+    while j>=0 and value < A[j]:
+        A[j+1] = A[j]
+        j -= 1
+        A[j+1] = value
+print("Sorted array")
+for i in range(len(A)):
+    print("%d"%A[i], end="")
 
 
-def swap(a):
-    if len(a) <= 1:
-        return
-    
-    x= -1
-    y= -1
-    prev = a[0]
-    for i in range(1,len(a)):
-        if prev > a[i]:
-            if x == -1:
-                x = i-1
-                y = i
-            else:
-                y = i
+A = [1,2,3,4,5,6,7,8,9]
+start = 0
+end = len(A) - 1
 
-        prev = a[i]
-    swap(a,x,y)
+while start < end:
+    A[start], A[end] = A[end], A[start]
+    start += 1
+    end -= 1    
 
-def swap(a,i,j):
-    temp = a[i]
-    a[i] = a[j]
-    a[j] = temp
+print(A)
 
-
-if __name__ == "__main__":
-    a = array
-    print("Original:", a)
-    swap(a)
-    print("sorted: ", a)
-
-
-            
-        
-            
-            
 
 
