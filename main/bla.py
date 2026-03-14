81040ac81ccca0affb3eaf00780842b6e608244e
Scooter X <ScooterX@hotmail.com>
2026-03-14 10:40:59 -0700

    Mar 14 2026

--- main/bla.py
+++ main/bla.py
@@ -1,16 +1,5 @@
-A = [10,5,13,8,2]
-for i in range(1,len(A)):
-    value = A[i]
-    j = i-1
-    while j>=0 and value < A[j]:
-        A[j+1] = A[j]
-        j -= 1
-        A[j+1] = value
-print("Sorted array")
-for i in range(len(A)):
-    print("%d"%A[i], end="")
-
-
-
-
-
+array = [1,6,3,7,4,6,4,3]
+a = max(array)
+array.remove(a) 
+b = max(array)
+print(a, b)
