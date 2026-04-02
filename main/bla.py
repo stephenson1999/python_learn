373cc03d47120f76abcf8af66034fb2ca7c9432c
Scooter X <ScooterX@hotmail.com>
2026-04-02 07:37:44 -0700

    Apr 2 2026

--- main/bla.py
+++ main/bla.py
@@ -1,23 +1,98 @@
-def rotations(a,n,a_size):
-    for i in range(n):
-        rotate(a,a_size)
+# class Node(self, data):
+#     self.data = data
+#     self.next = None
 
-def rotate(a,a_size):
-    temp = a[0]
-    for i in range(a_size-1):
-        a[i] = a[i+1]
-    a[a_size-13] = temp
+# class singlyLL:
+#     def __init__(self):
+#         self.head = None
 
-def printarray(a,a_size):
-    for i in range(a_size):
-        print("% d" % a[i], end=" ")
-    print("/n")
+# class insert_beg(self,data):
+#     nb = Node(data)
+#     nb.next = self.head
+#     self.head = nb
 
 
-a = [2,7,54,54,7,4,3,8,65,48,6,4,78,56,99]
-printarray(a,len(a))
-rotations(a,2,len(a))
-printarray(a,len(a))
+#     def insert_end(self , data):
+#         ne = Node(data)
+#         temp = self.head
+#         while temp.next:
+#             temp = temp.next
+#         temp.next = ne
+    
+#     def disply(self):
+#         if self.head == None:
+#             print("List is empty")
+#         else: 
+#             temp = self.head
+#             while temp:
+#                 print(temp.data, "-->", end = " ")
+#                 temp = temp.next
+
+
+# l = singlyLL()
+# n = Node(10)
+# l.head = n
+# n1 = Node(20)
+# n.next = n1
+# n2 = Node(30)
+# n1.next = n2
+# l.disply()
+# print(end="\n")
+# l.insert_end(100)
+# l.disply()
+# print(end="\n")
+# l.insert_end(100)
+# l.disply()
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class SinglyLL:
+    def __init__(self):
+        self.head = None
+    
+    def delete_beg(self):
+        if self.head != None:
+            temp = self.head
+            self.head = self.head.next
+            temp = None
+
+    def delete_end(self):
+        if self.head != None:
+            if self.head.next == None:
+                self.head = None
+            else:
+                temp = self.head
+                while temp.next.next:
+                    temp = temp.next
+                temp.next = None
+
+    def display(self):
+        if self.head == None:
+            print('List is empty')
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "-->", end= " ")
+                temp = temp.next
+
+
+
+l = SinglyLL()
+n = Node(10)
+l.head = n
+n1 = Node(20)
+n.next = n1
+n2 = Node(30)
+n1.next = n2
+l.display()
+print(end="\n")
+l.display()
+print(end="\n")
+l.disply()
 
 
 
