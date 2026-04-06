e1b76ff9a3ff6ced64ccb500ecfbd9ac3bbc842e
Scooter X <ScooterX@hotmail.com>
2026-04-06 07:32:31 -0700

    Apr 6 2026

--- main/bla.py
+++ main/bla.py
@@ -1,70 +1,70 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.next = None
 
-
-class LinkedList:
+class SingLL:
     def __init__(self):
         self.head = None
 
+    def swap(self,n1,n2):
+        prevNode1 = None
+        prevNode2 = None
+        node1 = self.head
+        node2 = self.head
 
-    def append(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
+        if (self.head == None):
             return
+        
+        if n1 == n2:
+            return
+        
+        while (node1 != None and node1.data != n1):
+            prevNode1 = node1
+            node1 = node1.next
 
-        current = self.head
-        while current.next:
-            current = current.next
+        while (node2 != None and node1.data != n2):
+            prevNode2 = node2
+            node2 = node2.next
 
-        current.next = new_node
+        if (node1 != None and node2 != None):
+
+            if(prevNode1 != None):
+                prevNode1.next = node2
+            else:
+                self.head = node2
+
+            if(prevNode2 != None):
+                prevNode2.next = node1
+            else:
+                self.head = node1
+
+            temp = node1.next
+            node1.next = node2.next
+            node2.next = temp
+        else:
+            print("Swaping is not possible")
+
 
     def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" → ")
-            current = current.next
-        print("None")
+        if self.head == None:
+            print("List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "--", end=" ")
+                temp = temp.next
 
-
-    def find(self, target):
-        current = self.head
-        position = 0
-
-        while current:
-            if current.data == target:
-                return position
-            current = current.next
-            position += 1
-
-        return -1  
-
-ll = LinkedList()
-
-ll.append(3)
-ll.append(7)
-ll.append(10)
-ll.append(15)
-
-print("Linked List:")
-ll.display()
-
-
-value = 10
-result = ll.find(value)
-
-if result != -1:
-    print(f"{value} found at position {result}")
-else:
-    print(f"{value} not found")
-
-value = 5
-result = ll.find(value)
-
-if result != -1:
-    print(f"{value} found at position {result}")
-else:
-    print(f"{value} not found")
+l = SingLL()
+n = Node(10)
+l.head = n
+n1 = Node(20)
+n.next = n1
+n2 = Node(30)
+n1.next = n2
+n3 = Node(40)
+n2.next = n3
+l.display()
+print(end="\n")
+l.swap(10,30)
+l.display()
