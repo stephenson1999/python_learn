e64a001c2856f33d92e64530cc8e0d8f8414e8af
Scooter X <ScooterX@hotmail.com>
2026-04-13 07:31:16 -0700

    Apr 13 2026

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
-array = [1,6,4,5,8]
 
-n = len(array)
-interval = n // 2
-while interval > 0:
-    for i in range(interval,n):
-        temp = array[i]
-        j = i
-        while j >= interval and array[j-interval] > temp:
-            array[j] = array[j - interval]
-            j -= interval
-        array[j] = temp
-    interval //= 2
+class DoubleLL:
+    def __init__(self):
+        self.head = None
 
-print(array)
+    def swap(self, n1, n2):
+        if self.head is None or n1 == n2:
+            return
+
+
+    def display(self):
+        if self.head is None:
+            print("List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "--", end=" ")
+                temp = temp.next
+
+l = DoubleLL()
+
+n = Node(10)
+l.head = n
+
+n1 = Node(20)
+n.next = n1
+
+n2 = Node(30)
+n1.next = n2
+
+n3 = Node(40)
+n2.next = n3
+
+l.display()
+
+l.swap(10, 30)
+l.display()
+
+l.swap(20, 40)
+l.display()
--- main/bla.py
+++ main/bla.py
@@ -2,25 +2,47 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.prev = None
 
 
-class DoubleLL:
+class SingLL:
     def __init__(self):
         self.head = None
 
-    def searching(self,data):
-        t = 0
-        temp = self.head
-        while temp:
-            if temp.data == data:
-                t = 1;
-                break;
-        if t == 1:
-            print('Element Found')
-        else:
-            print('Element not Found')
+        prevNode1 = None
+        prevNode2 = None
+        node1 = self.head
+        node2 = self.head
 
+        # Find node1
+        while node1 is not None and node1.data != n1:
+            prevNode1 = node1
+            node1 = node1.next
+
+        # Find node2
+        while node2 is not None and node2.data != n2:
+            prevNode2 = node2
+            node2 = node2.next
+
+        # If either node not found
+        if node1 is None or node2 is None:
+            print("Swapping is not possible")
+            return
+
+        # Update previous pointers
+        if prevNode1 is not None:
+            prevNode1.next = node2
+        else:
+            self.head = node2
+
+        if prevNode2 is not None:
+            prevNode2.next = node1
+        else:
+            self.head = node1
+
+        # Swap next pointers
+        temp = node1.next
+        node1.next = node2.next
+        node2.next = temp
 
     def display(self):
         if self.head is None:
@@ -30,15 +52,28 @@ class DoubleLL:
             while temp:
                 print(temp.data, "--", end=" ")
                 temp = temp.next
+            print()
 
-l = DoubleLL()
+
+
+l = SingLL()
+
 n = Node(10)
 l.head = n
+
 n1 = Node(20)
 n.next = n1
+
+n4 = Node(50)
+n1.next = n4
+
 n2 = Node(30)
-n1.prev = n
-n1.next = n2
+n4.next = n2
+
+n3 = Node(40)
+n2.next = n3
+
 l.display()
-print(end="\n")
-l.searching(20)
+
+
+
