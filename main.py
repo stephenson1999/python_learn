0e41c73833e9f92b767154f5bcbbc02a396b2777
Scooter X <ScooterX@hotmail.com>
2026-04-16 07:27:33 -0700

    Apr 16 2026

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
+kind = input("add or mutiplay?: ")
+
+x = [[8,2],
+    [4,1]]
+
+y = [[3,8],
+     [9,15]]
+
+answer = [[0,0], [0,0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        if kind == "add":
+            answer[i][j] = x[i][j] + y[i][j]
+        if kind == "mutiplay":
+            answer[i][j] = x[i][j] * y[i][j]
+
+print(answer)
 
 
-class DoubleLL:
-    def __init__(self):
-        self.head = None
 
-    def display(self):
-        if self.head is None:
-            print("List is empty")
-        else:
-            temp = self.head
-            while temp:
-                print(temp.data, "--", end=" ")
-                temp = temp.next
 
-l = DoubleLL()
 
-n = Node(10)
-l.head = n
 
-n1 = Node(20)
-n.next = n1
 
-n2 = Node(30)
-n1.next = n2
 
-n3 = Node(40)
-n2.next = n3
 
-l.display()
 
+
+
+
+
+
+
+
+
+
+Nasa_discoverys_of_2026="https://www.nasa.gov/blogs/watch-the-skies/2026/01/16/most-notable-2026-astronomical-events-a-year-of-watching-the-skies/"
