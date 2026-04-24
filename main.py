2c1332e053a6af845500367442cd99fd37c67f3e
Scooter X <ScooterX@hotmail.com>
2026-04-24 16:58:19 -0700

    Apr 24 2026

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-def reverse(s):
-    n = len(s)
-    li = list(s)
-    for i in range(n//2):
-        li[i], li[n-i-1] = li[n-i-1], li[i]
-        return "".join(li)
-        
-inp = input("Enter a string: ")
-print(reverse(inp))
+string = "Scott"
+a = string[:3]
+b = string[3:]
+print(b,a)
 
 
 
 
-inp = "apoisjf poeifj paowiejfiaopjpiejpieojpoipjefioejspfijseifjdaij"
-print(inp[:3])
-print(inp[3:])
-print(inp[1:5:2])
-print(inp[::-1])
-print(inp[-1:-9:-2])
-print(inp[-2:-9:-2])
+
+
+
+
+
+
+
 
