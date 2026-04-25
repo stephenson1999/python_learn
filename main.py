5475ead080ff284ee41402a3c1925df2b08e641a
Scooter X <ScooterX@hotmail.com>
2026-04-25 13:51:46 -0700

    Apr 25 2026

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-string = "Scott"
-a = string[:3]
-b = string[3:]
-print(b,a)
+a = [[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+
+answer = []
+
+for i in range(len(a)):
+    an = a[i][0] + a[i][1] + a[i][2]
+    answer.append(an)
+
+print(answer)
 
 
-
-
-
-
-
-
-
-
-
-
+        
