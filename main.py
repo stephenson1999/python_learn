516a96f77dc9f76ccd2bf31d1a07cb60e3251af2
Scooter X <ScooterX@hotmail.com>
2026-05-18 07:49:00 -0700

    May 18 2026

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-s = input("Enter a string: ")
-words = 0
+def iprice(barcode):
 
-for i in range(len(s)):
-    if s[i] == " ":
-        words += 1
+    l=[]
 
-words += 1
-print(words)
+    for i in barcode:
 
+        n=ord(i) #ordinal that returns a unicode
+
+        print(n)
+
+        if n//10: # if a num is > 9
+
+            max=0
+
+            while n>0:
+
+                if n % 10>max:
+
+                    max=n%10
+
+                    print(max)
+
+                    n=n//10
+
+                    print(n)
+
+                    l.append(max)
+
+                    print(l)
+
+                else:
+
+                    l.append(n)
+
+                    return sum(l)
+
+barcode=input("Enter")
+
+print(iprice(barcode))
 
