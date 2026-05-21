ee926825f6cc304a733fb1e31a70739abf624a0f
Scooter X <ScooterX@hotmail.com>
2026-05-21 07:37:09 -0700

    May 21 2026

--- Class Stuff/Rabin.py
+++ Class Stuff/Rabin.py
@@ -59,3 +59,91 @@ pattern = "CDD"
 q = 13
 
 searchPattern(pattern, text, q)
+
+
+
+
+def KMPpattern(pat, txt):
+    def LPSArray(pat, M, lps):
+
+        length = 0 # length of the previous longest prefix suffix
+
+        lps[0] = 0 # lps[0] is always 0
+
+        i = 1
+
+    M = len(pat)
+
+    N = len(txt)
+
+    # create lps[] that will hold the longest prefix suffix values for pattern
+
+    lps = [0] * M
+
+    j = 0 # index for pat[]
+
+    # Preprocess the pattern (calculate lps[] array)
+
+    LPSArray(pat, M, lps)
+
+    i = 0 # index for txt[]
+
+    while (N - i) >= (M - j):
+
+        if pat[j] == txt[i]:
+
+            i += 1
+
+            j += 1
+
+            if j == M:
+
+                print("Found pattern at index " + str(i - j))
+
+                j = lps[j - 1]
+
+        # mismatch after j matches
+
+        elif i < N and pat[j] != txt[i]:
+
+            if j != 0:
+
+                j = lps[j - 1]
+
+            else:
+
+                i += 1
+
+
+
+    # the loop calculates lps[i] for i = 1 to M-1
+
+    while i < M:
+
+        if pat[i] == pat[length]:
+
+            length += 1
+
+            lps[i] = length
+
+            i    += 1
+
+        else:
+
+            if length != 0:
+
+                length = lps[length - 1]
+
+            else:
+
+                lps[i] = 0
+
+    i += 1
+
+# Driver code (to show output)
+
+txt = input("Enter the text: ")
+
+pat = input("Enter the pattern: ")
+
+KMPpattern(pat, txt)
